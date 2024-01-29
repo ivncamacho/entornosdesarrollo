c2b89740482ad7c3caef58e56b2abed28bdb026d
smrv21-06 <smrv21-06@c16x13.iescierva.net>
2024-01-29 19:25:25 +0100

    Ejem01 variable con nombre y print de nombre

--- ejemplo1.java
+++ ejemplo1.java
@@ -1,5 +1,7 @@
 public class ejemplo1{
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		String nombre = "Ivan";
+		System.out.println("Mi nombre es " + nombre);
 	}
 }
